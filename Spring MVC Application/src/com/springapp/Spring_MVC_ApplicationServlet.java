04dcb7c13d42b5187bdb18d54f64f8f67de32131
sajediqbal <sajediqbal@gmail.com>
2015-04-17 02:00:12 +0500

    My First Commit

---
+++ Spring MVC Application/src/com/springapp/Spring_MVC_ApplicationServlet.java
@@ -0,0 +1,13 @@
+package com.springapp;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Spring_MVC_ApplicationServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
